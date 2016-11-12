a582030aa110fe448af42aad87682b4427a9bd6c
Nicolai <publicclubok@gmail.com>
2016-11-12 16:11:38 +0100

    Revert "just experimenting with git. added new comment"
    This reverts commit a5899b8a91cc02991e06e41783995b47eb3bf06a.

--- src/autoBus/AutoBusApp.java
+++ src/autoBus/AutoBusApp.java
@@ -12,7 +12,6 @@ public class AutoBusApp {
 	 */
 	public static void main(String[] args) throws Exception{
 		//test chauffeurArchive:
-		//new comment
 		ChauffeursArchive chauffeursArchive = new ChauffeursArchive();
 		/*
 		chauffeursArchive.addChauffeur(new Chauffeur("Mogens", "Bjerregaard"));
